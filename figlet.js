3ec7e1898f2aebc5e4648d8e58670fd397b98354
Lechandre <whitneycloete27@gmail.com>
2021-09-07 13:53:32 +0200

    initial commit

---
+++ figlet.js
@@ -0,0 +1,50 @@
+
+
+var figlet = require('figlet');
+
+figlet('Hello World!!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
+
+
+figlet.text('Boo!', {
+    font: 'Ghost',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 80,
+    whitespaceBreak: true
+}, function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
+
+
+console.log(figlet.textSync('Boo!', {
+    font: 'Ghost',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 80,
+    whitespaceBreak: true
+}));
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ greet.js
@@ -0,0 +1,27 @@
+
+
+module.exports = function(name){
+    return 'Hello, ' + name;
+}
+
+const greet = require('./greet');
+
+console.log(greet('Xola'))
+
+
+
+
+
+
+
+
+
+
+
+
+//function greet(name) {
+    //return 'Hello, ' + name;
+//}
+
+//var msg = greet('Xola')
+//console.log(msg)
